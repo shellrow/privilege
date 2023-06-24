8f188d3b7a01ff81b8a5df00e4e2cbb68f7c4cd7
shellrow <81893184+shellrow@users.noreply.github.com>
2023-06-24 16:55:02 +0900

    Update root_shell.rs

--- examples/root_shell.rs
+++ examples/root_shell.rs
@@ -1,5 +1,9 @@
 use privilege::runas::Command;
 
+/// Get the name of the shell to be run.
+///
+/// On Windows, it defaults to "cmd". On Unix-like systems, it uses the value
+/// of the SHELL environment variable, or "bash" if the variable is not set.
 fn get_shell_name() -> String {
     #[cfg(windows)]
     {
@@ -12,10 +16,11 @@ fn get_shell_name() -> String {
 }
 
 fn main() {
-    println!("Run a root shell ...");
+    println!("Running a root shell ...");
     let mut cmd: Command = Command::new(get_shell_name());
+
     match cmd.run() {
-        Ok(status) => println!("Exit Status: {}", status),
-        Err(e) => println!("Failed to execute. {}", e),
+        Ok(status) => println!("Shell exited with status: {}", status),
+        Err(e) => println!("Failed to execute shell with root privileges. {}", e),
     }
 }
